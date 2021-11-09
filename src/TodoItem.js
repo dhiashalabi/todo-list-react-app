d16d08feec30be4ffb37f5a3727f842330af3bfe
9iaashalaby <30384731+9iaashalaby@users.noreply.github.com>
2021-11-09 02:01:15 -0800

    Add Change event method handling

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -8,7 +8,7 @@ export default class TodoItem extends React.Component {
     render() {
         return (
             <div className="todo-item">
-                <input type="checkbox" checked={this.props.todo.completed} />
+                <input type="checkbox" checked={this.props.todo.completed} onChange={()=>{alert("Changed !")}}/>
                 <p>{this.props.todo.text}</p>
             </div>
         );
