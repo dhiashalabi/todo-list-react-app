5d80e3a3485c734dcf5e96ee6d0560bbb86c9d39
9iaashalaby <30384731+9iaashalaby@users.noreply.github.com>
2021-11-09 01:34:35 -0800

    Adding state for todos data

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React from "react";
 import TodoItem from "./TodoItem";
 import todosData from "./todosData";
 
-const todos = todosData.map(todo => <TodoItem key={todo.id} todo={todo} />)
 
 export default class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            todos: todosData
+        }
+    }
     render() {
+        const todos = this.state.todos.map(todo => <TodoItem key={todo.id} todo={todo} />)
+
         return (
             <div className="todo-list">
                 {todos}
